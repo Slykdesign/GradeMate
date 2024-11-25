f1a49a15e1608371c5329bebd5ae05ab49db395e
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2024-11-25 09:38:31 -0500

    Add files via upload

---
+++ Demo1/Data/Migrations/20241116225413_AddFullName.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YSUId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "YSUId",
+                table: "AspNetUsers");
+        }
+    }
+}
