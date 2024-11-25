97d416fe7c61aaea00ffed69b50fbf9ff523773c
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2024-11-25 09:32:01 -0500

    Create ApplicationDbContext.cs

---
+++ Demo1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Demo1.Areas.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
